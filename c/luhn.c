af6a6c0e368d5ff27a63c420dcffb6c2ad8bcb2b
Pedro Kitagawa <113566710+k1sta@users.noreply.github.com>
2023-05-05 02:37:42 +0000

    Algoritmo de Luhn, CC50

--- c/luhn.c
+++ c/luhn.c
@@ -1,40 +1,121 @@
 #include <stdio.h>
 #include <math.h>
+#define VISACODE 0
+#define AMEXCODE 1
+#define MASTERCARDCODE 2
+#define ERRORCODE 3
 
-int checksum(int digit[]){
+int checksum(int digit[], int n){
+    int sum = 0, temp;
+    
+    for(int j = n - 2; j >= 0; j -= 2){
+        if (digit[j] * 2 >= 10){
+            temp = digit[j] * 2;
+            while (temp != 0){
+                sum += temp % 10;
+                temp = floor(temp / 10);
+            }
+        } else{
+            sum += digit[j] * 2;
+        }
+    }
 
+    for(int k = n - 1; k >= 0; k -= 2){
+        sum += digit[k];
+    }
+
+    if (sum % 10 == 0){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int card_flag(int digit[]){
+    if (digit[0] == 4){ // visa
+        return VISACODE;
+    } else if (digit[0] == 3 && (digit[1]==4 || digit[1]==7)){ // amex
+        return AMEXCODE;
+    } else if (digit[0] == 5 && (digit[1] > 0 || digit[1] <= 5)){ // master card
+        return MASTERCARDCODE;
+    } else {
+        return ERRORCODE;
+    }
 }
 
 int main(){
-    long int card_number;
-    int card_flag;
+    long long card_number_var;
+    int n = 0, flag_code;
 
     printf("Type your credit card number: ");
-    scanf("%li", &card_number);
+    scanf("%lld", &card_number_var);
 
-    int n = floor(log10(abs(card_number)));
+    const long long card_number_const = card_number_var;
+
+    while (card_number_var != 0){
+        card_number_var = floor(card_number_var / 10);
+        n++;
+    } card_number_var = card_number_const;
+
     int digit[n];
 
-    for(int i = n -1; i > 0; i--){
-        digit[i] = card_number % 10;
-        card_number = floor(card_number / 10);
-    }
+    for(int i = n - 1; i >= 0; i--){
+        digit[i] = card_number_var % 10;
+        card_number_var = floor(card_number_var / 10);
+    }   card_number_var = card_number_const;
 
-    if (digit[0] = 4){
-        if (n = 13 || n = 16){
-            card_flag = 1;
 
+    flag_code = card_flag(digit);
+    
+    switch (flag_code)
+    {
+    case VISACODE:
+        if (n == 13 || n == 16){
+            if (checksum(digit, n) == 0){
+                printf("VISA\n");
+            } else {
+                printf("INVALID\n");
+                return 3;
+            }
         } else {
-
+            printf("INVALID\n");
+            return 2;
         }
+        break;
+    
+    case AMEXCODE:
+        if (n == 15){
+            if (checksum(digit, n) == 0){
+                printf("AMEX\n");
+            } else {
+                printf("INVALID\n");
+                return 3;
+            }
+        } else {
+            printf("INVALID\n");
+            return 2;
+        }
+        break;
 
-    } else if (digit[0] = 5 ){
-
-    } else if (digit[0] = 3){
-
-    } else{
-        printf("INVALID");
+    case MASTERCARDCODE:
+        if (n == 16){
+            if (checksum(digit, n) == 0){
+                printf("MASTERCARD\n");
+            } else {
+                printf("INVALID\n");
+                return 3;
+            }
+        } else {
+            printf("INVALID\n");
+            return 2;
+        }
+        break;
+            
+    default:
+        printf("INVALID\n");
+        return 1;
     }
+
 
     return 0;
 }
