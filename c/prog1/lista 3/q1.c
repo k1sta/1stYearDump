2c6eefc4d06f089cd1cc0759656d392a59d5f4a9
Pedro Kitagawa <113566710+k1sta@users.noreply.github.com>
2023-05-19 19:06:02 +0000

    aswed ijuhb

--- c/prog1/lista 3/q1.c
+++ c/prog1/lista 3/q1.c
@@ -1,10 +1,16 @@
+/*
+Descricao: esse codigo calcula a raiz quadrada de um numero baseado no metodo de newton e compara com o resultado da funcao do math.h
+Entrada: um número racional
+Saida: o número inputado + a raiz dele de acordo com a funcao sqrt() + a raiz dele de acordo com o metodo de newton
+defesa: nao aceita numero negativo e retorna 0 caso o input seja 0 por causa da imprecisao
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 
 //prototipo
-double dif_abs(double x, double y);
+double d_abs(double x);
 
 
 int main(){
@@ -12,15 +18,18 @@ int main(){
     double n, x_novo, x_antigo = 10; //chute
 
     //pegando o numero
+    do{
     printf("Digite o número: ");
     scanf("%lf", &n);
+    if (n >= 0) break;
+    } while(1);
 
     //calculando
-    while(1){
+    if (n != 0) while(1){
         x_novo = x_antigo - ((x_antigo * x_antigo) - n)/(2 * x_antigo);
-        if (dif_abs(x_novo, x_antigo) < 0.000001) break;
+        if (d_abs(x_novo - x_antigo) < 0.000001) break;
         x_antigo = x_novo;
-    }
+    };
     
     //impressao
     printf("Numero lido: %lf \nRaiz da math.h: %lf \nRaiz de Newton: %lf\n", n, sqrt(n), x_novo);
@@ -31,7 +40,7 @@ int main(){
 
 
 //essa funcao faz o que o abs nao faz (aceita double)
-double dif_abs(double x, double y){
-    if (x - y < 0) return (x - y) * -1;
-    return x - y;
+double d_abs(double x){
+    if (x < 0) return x * (-1);
+    return x;
 }
