b673ad01fb6e393dd486b3fb1826ee2740cbb43a
Pedro Kitagawa <113566710+k1sta@users.noreply.github.com>
2023-06-14 16:40:40 -0300

    Atualizar o atividade3.c
    teste teste

--- c/prog1/lab 8/atividade3.c
+++ c/prog1/lab 8/atividade3.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    int orig[10000], ord[10000];
-    
-    while()
+  int counter, aux, size_vec, orig[1000], ord[1000];
 
+  puts("Insert the number of the array: ");
+  scanf("%d", &size_vec);
+
+  printf("Insert %d values: \n", size_vec);
+  for(int i = 0; i < size_vec; i++){
+    scanf("%d", &orig[i]);
+    ord[i] = orig[i];
+  }
+
+  do{
+    counter = 0;
+    for(int i = 0; i < size_vec; i++){
+      if (ord[i-1] > ord[i]){
+        aux = ord[i];
+        ord[i] = ord[i-1];
+        ord[i-1] = aux;
+      } else counter++;
+      
+    }
+  } while(counter != size_vec-1);
+  
+  for(int i = 0; i < size_vec; i++){
+    printf("%d", ord[i]);
+  }
+  
+  printf("\n");
+
+  return 0;
+    
+    
 }
