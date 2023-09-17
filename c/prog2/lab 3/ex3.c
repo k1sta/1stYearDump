1cec9d91dd19d6e7997456bef4f90d4d22fb58fd
Pedro Kitagawa <113566710+k1sta@users.noreply.github.com>
2023-09-17 16:45:09 +0000

    lab 3 revisado & comentado

--- c/prog2/lab 3/ex3.c
+++ c/prog2/lab 3/ex3.c
@@ -3,22 +3,6 @@
 #define MAX 1000
 
 int main (int argc, char **argv){
-    char cin[MAX];
-
-    FILE *arquivoEntrada = fopen(argv[1], "r");
-    if (arquivoEntrada == NULL){
-        printf("nao abriu o arquivo bobao1\n");
-        return -1;
-    }
-    FILE *arquivoSaida = fopen(argv[2], "w+");
-    if(arquivoSaida == NULL){
-        printf("nao abriu o arquivo bobao2");
-        return -1;
-    }
-
-    while(!feof(arquivoEntrada)){
-        fgets(cin, MAX, arquivoEntrada);
-    }
     
     return 0;
 }
